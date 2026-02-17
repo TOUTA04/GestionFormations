7d29db338985134f4ba8bf974897e4c4fd29cb07
TOUTA04 <aya.benzarti@edu.isetcom.tn>
2026-02-17 12:30:35 +0100

    Add files via upload

--- GestionFormations/profil.php
+++ GestionFormations/profil.php
@@ -1,3 +1,19 @@
 <?php
-echo "Page Profil PHP";
+$nom = "AYARI";
+$prenom = "Asma";
+$email = "asma.ayari@email.com";
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>Profil utilisateur</title>
+</head>
+<body>
+<h1>Profil utilisateur</h1>
+<p><strong>Nom :</strong> <?= $nom ?></p>
+<p><strong>Prénom :</strong> <?= $prenom ?></p>
+<p><strong>Email :</strong> <?= $email ?></p>
+</body>
+</html>
+<p>Date : <?= date("H:i:s") ?></p>
